98f079e6bb5bb5c7922d3e526e0acad97b8c5d7b
Andres Armendariz <119715511+AEArmendarizB@users.noreply.github.com>
2023-02-09 21:34:00 -0500

    Se arreglo un error con las rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -12,13 +12,13 @@ import { TransferenciasComponent } from './Components/transferencias/transferenc
 const routes: Routes = [
     { path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent},
-    { path: '/menu', component: MenuPrincipalComponent},
-    { path: '/usuario', component: NuevasCredencialesComponent},
-    { path: '/pregunta', component: PreguntaSeguridadComponent},
-    { path: '/registro-cliente', component: RegistroClienteComponent},
-    { path: '/transferencia', component: TransferenciasComponent},
-    { path: '/transferencia-interna', component: TransferenciasInternasComponent},
-    { path: '/transferencia-interna-resumen', component: TransferenciasInternasResumenComponent},
+    { path: 'menu', component: MenuPrincipalComponent},
+    { path: 'usuario', component: NuevasCredencialesComponent},
+    { path: 'pregunta', component: PreguntaSeguridadComponent},
+    { path: 'registro-cliente', component: RegistroClienteComponent},
+    { path: 'transferencia', component: TransferenciasComponent},
+    { path: 'transferencia-interna', component: TransferenciasInternasComponent},
+    { path: 'transferencia-interna-resumen', component: TransferenciasInternasResumenComponent},
     { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
