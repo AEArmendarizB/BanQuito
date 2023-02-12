a49fccbecd129f3c0588bc254aa6ac1ca04e0023
Emerson Solis <emerson1524ricardo@gmail.com>
2023-02-12 14:38:11 -0500

    Implementación de la interfaz de login

--- frontend/src/app/Components/nuevas-credenciales/nuevas-credenciales.component.ts
+++ frontend/src/app/Components/nuevas-credenciales/nuevas-credenciales.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginUsuario } from 'src/app/models/login.usuario';
 
 @Component({
   selector: 'app-nuevas-credenciales',
   templateUrl: './nuevas-credenciales.component.html',
   styleUrls: ['./nuevas-credenciales.component.css']
 })
-export class NuevasCredencialesComponent {
+export class NuevasCredencialesComponent implements OnInit{
+  title = 'Nuevas credenciales BanQuito';
 
+  public nuevo_usuario:LoginUsuario;
+
+  constructor(){
+    this.nuevo_usuario = new LoginUsuario('','');
+  }
+  
+  ngOnInit(): void {
+      
+  }
 }
