c177f1b030a64cf463d10722c3771b4ce255e676
Christian Satama <christian.satama@epn.edu.ec>
2023-02-11 23:15:18 -0500

    Control basico de <obligatorio> en los campos del formulario de Cliente

--- frontend/src/app/Components/registro-cliente/registro-cliente.component.ts
+++ frontend/src/app/Components/registro-cliente/registro-cliente.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Cliente } from 'src/app/models/clientes';
 
 @Component({
   selector: 'app-registro-cliente',
   templateUrl: './registro-cliente.component.html',
   styleUrls: ['./registro-cliente.component.css']
 })
-export class RegistroClienteComponent {
+export class RegistroClienteComponent implements OnInit {
+  formularioCliente: FormGroup;
+
+  constructor(private fb: FormBuilder, 
+    private toastr: ToastrService){
+      //Cliente
+    this.formularioCliente = this.fb.group({
+      nombres:  ['',Validators.required],
+      apellidos: ['',Validators.required],
+      cedula: ['',Validators.required],
+      codDactilar: ['',Validators.required],
+      fechaNacimiento: ['',Validators.required],
+      email: ['',Validators.required],
+      domicilio: ['',Validators.required],
+      ocupacion: ['',Validators.required],
+      numeroTelefono: ['',Validators.required]
+    });
+  }
+  agregarCliente(){
+    const CLIENTE: Cliente = {
+      nombres: this.formularioCliente.get('nombres')?.value,
+      apellidos: this.formularioCliente.get('apellidos')?.value,
+      cedula: this.formularioCliente.get('cedula')?.value,
+      codigo_dactilar: this.formularioCliente.get('codDactilar')?.value,
+      fecha_nacimiento: this.formularioCliente.get('fechaNacimiento')?.value,
+      correo_electronico:this.formularioCliente.get('email')?.value,
+      direccion: this.formularioCliente.get('domicilio')?.value,
+      ocupacion: this.formularioCliente.get('ocupacion')?.value,
+      numero_telefono: this.formularioCliente.get('numeroTelefono')?.value
+    }
+    console.log(CLIENTE);
+    this.toastr.info('El Cliente se registro con exito!','Cliente registrado');
+  }
+  ngOnInit(): void {
+  }
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -29,7 +30,10 @@ import { PreguntaSeguridadComponent } from './Components/pregunta-seguridad/preg
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
