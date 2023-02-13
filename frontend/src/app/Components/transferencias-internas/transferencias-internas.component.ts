220b824abe18b8803868ffe22a8be032487b7318
Andres Armendariz <119715511+AEArmendarizB@users.noreply.github.com>
2023-02-13 14:17:16 -0500

    Se crea instancias de control de formulario

--- frontend/src/app/Components/transferencias-internas/transferencias-internas.component.ts
+++ frontend/src/app/Components/transferencias-internas/transferencias-internas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transferencias-internas',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./transferencias-internas.component.css']
 })
 export class TransferenciasInternasComponent {
+  TraInternaForm: FormGroup;
 
+
+  constructor(private fb: FormBuilder){
+    this.TraInternaForm = this.fb.group({
+      cuentaOrigen: ['',Validators.required],
+      monto: ['',Validators.required],
+      cuentaDestino: ['',Validators.required],
+      descripcion: ['',Validators.required],
+    });
+  }
 }
