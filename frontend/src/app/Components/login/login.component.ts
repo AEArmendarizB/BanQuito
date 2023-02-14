d4953db4de3f44d02af84be549e8e292be6f79f0
Emerson Solis <emerson1524ricardo@gmail.com>
2023-02-14 00:02:03 -0500

    Se arreglaron los problemas de compatibilidad

--- frontend/src/app/Components/login/login.component.ts
+++ frontend/src/app/Components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginUsuario } from 'src/app/models/login.usuario';
-//import { Cliente } from 'src/app/models/usuarios';
+import { Usuario } from 'src/app/models/usuarios';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -14,24 +14,41 @@ export class LoginComponent implements OnInit{
   title = 'BanQuito';
   public myForm!:FormGroup;
 
-  public login_usuario:LoginUsuario;
+  public login_usuario:Usuario;
 
   //usuarioForm: FormGroup;
 
   constructor(private fb: FormBuilder,private router: Router, private _usuarioService: UsuarioService ){
-    this.login_usuario = new LoginUsuario('','');
-    
-
-
+    this.login_usuario = new Usuario(0,'','','');
   }
 
   ngOnInit(): void {
       //this.myForm = this.createMyForm();
+      this.myForm = this.createMyForm();
+  }
+
+  private createMyForm():FormGroup{
+    return this.fb.group({
+      usuario:['',Validators.required],
+      password:['',Validators.required]
+    });
+  }
+
+  public submitFormulario(){
+    if(this.myForm.invalid){
+      Object.values(this.myForm.controls).forEach(control=>{
+        control.markAllAsTouched();
+      });
+      return;
+    }
+    console.log(this.myForm.value);
+  }
+
+  public get f():any{
+    return this.myForm.controls;
   }
 
   verificarUsuario(){
-    
-    
     
     this._usuarioService.verificarUsuario(this.login_usuario).subscribe(data =>{
       
--- frontend/src/app/services/usuario.service.ts
+++ frontend/src/app/services/usuario.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { LoginUsuario } from "../models/login.usuario";
+import { Usuario } from "../models/usuarios";
 
-import { Cliente } from "../models/usuarios";
+//import { Cliente } from "../models/usuarios";
 
 
 
@@ -20,7 +20,7 @@ export class UsuarioService{
 
     constructor(private http: HttpClient) { }
 
-    verificarUsuario(usuario:  LoginUsuario): Observable <any>{
+    verificarUsuario(usuario:  Usuario): Observable <any>{
         
         
         
