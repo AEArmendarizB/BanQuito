d967d0d5372edea415370e71960ccb5e4c2d5727
Christian Satama <christian.satama@epn.edu.ec>
2023-02-15 01:01:33 -0500

    Se agrego el metodo para poder realizar transferencias internernas, implementado unicamente sobre backend

--- backend/routes/clientes.routes.js
+++ backend/routes/clientes.routes.js
@@ -6,28 +6,12 @@ var multiparty=require('connect-multiparty');
 var mutipartyMiddleWare=multiparty({uploadDir:'./uploads'});
 //pagina de inicio
 router.get('/inicio',clientesController.inicio);
-//guardar un libro
+//guardar una cuenta
 router.post('/guardar-cliente',clientesController.saveCliente);
-//ver todos los libros
+//ver todas las cuentas
 router.get('/clientes',clientesController.getClientes);
 
 //validar cedula
 router.post('/validarCedula',clientesController.validarCedula);
 
-
-/*
-//ver datos de un libro
-router.get('/libro/:id',librosController.getLibro);
-//eliminar un libro
-router.delete('/libro/:id',librosController.deleteLibro);
-//actulizar un libro
-router.put('/libro/:id',librosController.updateLibro);
-//agregar una imagen
-router.post('/subir-imagen/:id',mutipartyMiddleWare,librosController.uploadImage);
-//recuperar la imagen
-router.get('/get-imagen/:imagen',librosController.getImagen);
-/*router.post
-router.put
-router.delete
-*/
 module.exports=router;
