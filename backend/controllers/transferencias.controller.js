8d547efb989091e91f36b217070965a37866cb1c
Andres Armendariz <119715511+AEArmendarizB@users.noreply.github.com>
2023-03-08 21:46:40 -0500

    Eliminacion de código repetido

--- backend/controllers/transferencias.controller.js
+++ backend/controllers/transferencias.controller.js
@@ -6,7 +6,6 @@ const path = require('path');
 var controller = {
     saveTransferencia:function(req, res){
         var transferencia =new Transferencia();
-        console.log('pipipi');
         var params = req.body;
         transferencia.cuentaEmisor = params.cedulaEmisor;
         transferencia.tipo_cuentaEmisor = params.tipo_cuentaEmisor;
@@ -17,8 +16,6 @@ var controller = {
         transferencia.tipo_cuentaReceptor = params.tipo_cuentaReceptor;
         transferencia.nombresReceptor = params.nombresReceptor;
         transferencia.apellidosReceptor = params.apellidosReceptor;
-        console.log('pipipi');
-        console.log(transferencia);
         transferencia.save((err, clienteGuardado) => {
             if (err) return res.status(500).send({ message: 500 });
             if (!clienteGuardado) return res.status(404).send({ message: 404 });
